6822044266877b109e8b9a548507ef0be0e4d91f
Unai Gonzalez <ugonzalez@dogfydiet.com>
2025-01-27 19:05:04 +0100

    Update changeLogs get function

--- src/api/admin/admin.handlers.js
+++ src/api/admin/admin.handlers.js
@@ -5,23 +5,21 @@ import mongoose from 'mongoose'
 const ChangeLogs = mongoose.model('ChangeLog')
 
 export async function getChangelogs (req, reply) {
-  const page = Number(req.query.page) || 1
-  const limit = Number(req.query.limit) || 10
-
-  const skip = (limit * page) - limit
   const { logId } = req.params
 
-  const [changeLog, changesResult] = await Promise.all([
-    ChangeLogs.findOne({ _id: logId }, { changes: { $slice: [skip, limit] } }).populate('changes.updatedBy').lean(),
-    ChangeLogs.findOne({ _id: logId }).select('changes').lean()
-  ])
-
-  const changes = changesResult ? changesResult.changes : []
+  const changeLog = await ChangeLogs.aggregate([
+      { $match: { _id: logId } },
+      { $unwind: '$changes' },
+      { $sort: { 'changes.updatedAt': -1 } },
+      { $lookup: { from: 'sysusers', localField: 'changes.updatedBy', foreignField: '_id', as: 'changes.updatedBy' } },
+      { $unwind: '$changes.updatedBy' },
+      { $group: { _id: '$_id', changes: { $push: '$changes' } } },
+    ])
 
   if (!changeLog) return reply.notFound('Changelog not found.')
 
   return {
-    docs: changeLog,
-    changesCount: changes.length,
+    docs: changeLog[0].changes,
+    changesCount: changeLog[0].length,
   }
 }
