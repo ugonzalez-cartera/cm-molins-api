b276c7dd5886345eb3035d831219627aadb8b7ff
Unai Gonzalez <ugonzalez@dogfydiet.com>
2025-03-17 18:20:21 +0100

    Update imports

--- src/api/investees/investees.routes.js
+++ src/api/investees/investees.routes.js
@@ -1,7 +1,7 @@
 import investeesHandlers from './investees.handlers.js'
 
-import { configAllowance } from '../services/authorization.service.js'
-import config from '../config.js'
+import { configAllowance } from '../../services/authorization.service.js'
+import config from '../../config.js'
 
 async function routes (fastify, opts) {
   // Set global authorization config.
