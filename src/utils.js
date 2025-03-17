e6e1ea4aed051d7630d6cbdfba836c497270d708
Unai Gonzalez <ugonzalez@dogfydiet.com>
2025-03-17 18:05:24 +0100

    Delete unnecesary comments utls custom error class

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,6 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/es.js'
 
 // --------------------
-/**
- * Parses a date and returns the month and year.
- * @param {string|Date} date - The date to parse.
- * @returns {{month: string, year: string}} The parsed month and year.
- */
 export function getParsedDate(date) {
   const month = dayjs(date).locale('es').format('MMMM').toUpperCase()
   const year = dayjs(date).format('YYYY')
@@ -18,36 +13,7 @@ export function getParsedDate(date) {
 // --------------------
 // This custom Error class is used to handle errors in the API.
 // https://www.rfc-editor.org/rfc/rfc7807
-
-/**
- * Custom error class for API errors.
- */
 export class CustomError extends Error {
-  /**
-   * Creates a new CustomError.
-   * @param {Object} params - The error parameters.
-   * @param {string} params.title - The error title.
-   * @param {string} params.detail - The error detail.
-   * @param {number} params.status - The HTTP status code.
-   * @param {string} [params.type] - The error type.
-   * @param {string} [params.instance] - The error instance.
-   * @param {string} [params.code] - The error code.
-   */
-  constructor({ title, detail, status, type = 'about:blank', instance = '', code = '' }) {
-    super(title)
-    this.name = this.constructor.name
-    this.title = title
-    this.detail = detail
-    this.status = status
-    this.type = type
-    this.instance = instance
-    this.code = code
-  }
-
-  /**
-   * Converts the error to a JSON object.
-   * @returns {Object} The JSON representation of the error.
-   */
   toJSON() {
     return {
       name: this.name,
@@ -60,9 +26,6 @@ export class CustomError extends Error {
     }
   }
 
-  /**
-   * Prints the error to the console.
-   */
   print() {
     console.error(
       `\n =================== \n` +
